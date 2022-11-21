605af1638e84077830e81e66d40f25475ea23986
romeusorionaet <romeusoares14569@gmail.com>
2022-11-21 11:47:48 -0300

    first commit

---
+++ js/favorites.js
@@ -0,0 +1,111 @@
+import { GithhubUser } from "./githubUsers.js"
+
+export class Favorites {
+    constructor(root) {
+        this.root = document.querySelector(root)
+        this.load()
+    }
+
+    load() {
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
+
+    save() {
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+        try {
+
+            const userExists = this.entries.find(entry => entry.login.toLowerCase() === username.toLowerCase())
+
+            if(userExists){
+                throw new Error('Usuário já cadastrado.')
+            }
+
+            const user = await GithhubUser.search(username)
+            if(user.login === undefined){
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        } catch(error){
+            alert(error.message)
+        }
+    }
+
+    delete(user) {
+        const filteredEntries = this.entries
+        .filter(entry => entry.login !== user.login)
+        this.entries = filteredEntries
+        this.update()
+    }
+
+}
+
+export class FavoritesView extends Favorites {
+    constructor(root) {
+        super(root)
+        this.tbody = this.root.querySelector('table tbody')
+        this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+            this.add(value)
+        }
+    }
+
+    update() {
+        this.removeAllTr()
+        this.entries.forEach(user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+                if(isOk){
+                    this.delete(user)
+                }
+            }
+
+            this.tbody.append(row)
+        })
+    }
+
+    removeAllTr() {
+        this.tbody.querySelectorAll('tr').forEach((tr) => {tr.remove()})
+    }
+
+    createRow() {
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <td class="user">
+            <img src="link da imagem do usuário.png" alt="nome do usuário">
+            <a href="link do github do usuário" target="_blank"> 
+                <p>nome do usuário</p>
+                <span>nome do usuário</span>
+            </a>
+            </td>
+            <td class="repositories"> </td>
+            <td class="followers"> </td>
+            <td>
+                <button class="remove">Remove</button>
+        </td>
+        `
+        return tr
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+## Projeto GitFavorites
+
+![preview](./img/preview.png)
+
+> Com esse projeto voçe pode favoritar qualquer pessoa do github, pesquisando seu nome irá aparecer na lista abaixo podendo entrar no seu perfil no github e poder se conectar.
+
+# Tecnologias utilizadas
+- HTML
+- CSS
+- JavaScript
+
+# Vamos nos conectar!
+- [linkdin⭐](https://www.linkedin.com/in/romeu-soares-87749a231/)
+
+- [Instagram⭐](http://instagram.com/romeusoaresdesouto)
